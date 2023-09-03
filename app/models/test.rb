429fb85b54ff6dfc3fbf514670d41b6795ac7a47
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-09-03 23:20:43 +0900

    test.rbとtest_spec.rbを削除

--- app/models/test.rb
+++
@@ -1,9 +0,0 @@
-class Calculate
-  def initialize(num)
-    @num = num
-  end
-
-  def half_num
-    @num / 2
-  end
-end
--- spec/models/test_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../../app/models/test'
-
-RSpec.describe Calculate do
-  it '1000の半分は500になる' do
-    calculate = Calculate.new(1000)
-    expect(calculate.half_num).to ebq 500
-  end
-end
