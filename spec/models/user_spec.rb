1b0aaea161b9c94f405eb751faf61376e1ca888c
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-09-03 23:22:18 +0900

    user_spec.rbを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require_relative '../rails_helper'
 
 RSpec.describe User, type: :model do
   it "is valid with valid attributes" do
-    binding.irb
     user = FactoryBot.build(:user)  # Userモデルのインスタンスを生成
     expect(user).to be_valid
   end
