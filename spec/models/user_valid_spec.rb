97ab26e842337cfcb3f485700d4870ab6b95bbbe
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-09-12 23:44:53 +0900

    userモデルのテストケース修正

--- spec/models/user_valid_spec.rb
+++ spec/models/user_valid_spec.rb
@@ -1,46 +1,68 @@
 RSpec.describe User do
   describe '#presenseメソッド' do
     context 'emailが存在する場合' do
+      let(:user) { build(:user, email: 'hoge@example.com') }
+
       it 'valid?がtrueになる' do
-        user = User.new(email: 'hoge@example.com', password: 'password')
         expect(user.valid?).to eq true
       end
     end
 
     context 'emailが空文字の場合' do
+      let(:user) { build(:user, email: '') }
+
+      it 'valid?がfalseになる' do
+        expect(user.valid?).to eq false
+      end
+
       it 'errorsに「メールアドレスを入力してください」と格納される' do
-        user = User.new(email: '', password: 'password')
         user.valid?
-        expect(user.errors.full_messages).to include("メールアドレスを入力してください")
+        expect(user.errors.full_messages).to include('メールアドレスを入力してください')
       end
     end
 
     context 'emailがnilの場合' do
+      let(:user) { build(:user, email: nil) }
+
+      it 'valid?がfalseになる' do
+        expect(user.valid?).to eq false
+      end
+
       it 'errorsに「メールアドレスを入力してください」と格納される' do
-        user = User.new(email: nil, password: 'password')
         user.valid?
-        expect(user.errors.full_messages).to include("メールアドレスを入力してください")
+        expect(user.errors.full_messages).to include('メールアドレスを入力してください')
       end
     end
 
     context 'passwordが存在する場合' do
+      let(:user) { build(:user, password: 'hogehoge') }
+
       it 'valid?がtrueになる' do
-        user = User.new(email: 'hoge@example.com', password: 'password')
         expect(user.valid?).to eq true
       end
     end
 
     context 'passwordが空文字の場合' do
+      let(:user) { build(:user, password: '') }
+
+      it 'valid?がfalseになる' do
+        expect(user.valid?).to eq false
+      end
+
       it 'errorsに「パスワードを入力してください」と格納される' do
-        user = User.new(email: 'hoge@example.com', password: '')
         user.valid?
         expect(user.errors.full_messages).to include("パスワードを入力してください")
       end
     end
 
     context 'passwordがnilの場合' do
+      let(:user) { build(:user, password: nil) }
+
+      it 'valid?がfalseになる' do
+        expect(user.valid?).to eq false
+      end
+
       it 'errorsに「パスワードを入力してください」と格納される' do
-        user = User.new(email: 'hoge@example.com', password: nil)
         user.valid?
         expect(user.errors.full_messages).to include("パスワードを入力してください")
       end
