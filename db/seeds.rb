a530545b09f09e38c401098df505d86d29e7db5f
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-09-13 22:50:03 +0900

    seeds.rbを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 # coding: utf-8
 
-User.create(email:'kaito@example.com', password:'hogehoge', created_at:'2023-08-09' ,updated_at:'2023-08-31')
-User.create(email:'shoichi@example.com', password:'hohogege', created_at:'2023-08-10' ,updated_at:'2023-09-01')
+User.create(email:'kaito@example.com', password:'hogehoge')
+User.create(email:'shoichi@example.com', password:'hohogege')
