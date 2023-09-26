fe8392f2a702487f10a24fd36050ad37a9625dc7
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-09-26 23:29:06 +0900

    flashメッセージ表示の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = '登録に成功しました'
+      flash[:notice] = '登録に成功しました'
       redirect_to '/users'
     else
-      flash.now[:alret] = '登録に失敗しました'
+      flash.now[:alert] = '登録に失敗しました'
       render :new
-      # redirect_to ('/users/new'), flash: {alret: '登録に失敗しました'}
+      flash.now[:alert] = '登録に失敗しました'
+
     end
   end
 
