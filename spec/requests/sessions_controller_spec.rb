905564c12af90f4b5418e9105b9d5769cf596957
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-11-27 11:03:47 +0900

    ロウアウト機能のRSpecを作成

--- spec/requests/sessions_controller_spec.rb
+++ spec/requests/sessions_controller_spec.rb
@@ -33,4 +33,32 @@ describe 'POST /login' do
       end
     end
   end
+
+  describe 'DELETE /logout' do
+    context 'ログインしている場合' do
+      let!(:user) { create(:user) }
+  
+      it 'ログアウトに成功すること' do
+        aggregate_failures do
+          post login_path, params: { email: user.email, password: 'sample_password' }
+          expect(response).to have_http_status(302)
+          expect(response).to redirect_to(user_path(user.id))
+          delete logout_path
+          expect(response).to have_http_status(302)
+          expect(response).to redirect_to login_path
+          expect(session[:user_id]).to eq nil
+          delete logout_path
+        end
+      end
+    end
+  end
+
+  context 'ログインしていない場合' do
+    let!(:user) { create(:user) }
+
+    it 'ログアウトに失敗すること' do
+      aggregate_failures do
+      end
+    end
+  end
 end
