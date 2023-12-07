1f4e5e70f9ea6d4d570343eeee83e92fd9569e77
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-12-07 21:30:57 +0900

    Tweetコントローラのnew,createアクションを実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,4 +11,29 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
     @user = @tweet.user
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    # ログイン機能がマージされていないので、仮にuser_id = 1 を代入してツイートを作成
+    @tweet.user_id = 1
+    # ログイン機能がマージされた後、以下を実装する
+    # @tweet.user_id = current_user.id
+    if @tweet.save
+      flash[:notice] = '投稿に成功しました'
+      redirect_to tweets_path
+    else
+      flash.now[:alert] = '投稿に失敗しました'
+      render :new
+    end
+  end
+
+  private
+    def tweet_params
+      binding.pry
+      params.require(:tweet).permit(:tweet_content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :new, :create, :show, :destroy, :edit, :update]
+<<<<<<< HEAD
   resources :tweets, only: [:index, :show]
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+=======
+  resources :tweets, only: [:index, :show, :new, :create]
+>>>>>>> 6c8875d (Tweetコントローラのnew,createアクションを実装)
 end
