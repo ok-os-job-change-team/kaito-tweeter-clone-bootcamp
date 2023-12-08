ceee102703ca896c3270866f585a16d85306ec12
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-12-08 23:44:38 +0900

    routes.rbを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :new, :create, :show, :destroy, :edit, :update]
-<<<<<<< HEAD
-  resources :tweets, only: [:index, :show]
-
+  resources :tweets, only: [:index, :show, :new, :create]
+  
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-=======
-  resources :tweets, only: [:index, :show, :new, :create]
->>>>>>> 6c8875d (Tweetコントローラのnew,createアクションを実装)
 end
