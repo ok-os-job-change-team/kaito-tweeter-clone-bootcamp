a7e1db92c90fcf866430770009a0b78baa075407
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-12-08 16:20:18 +0900

    Tweetsコントローラーにログインの判定を追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :check_logged_in, only: [:index, :show]
 
   def index
     @tweets = Tweet.all
@@ -7,5 +8,15 @@ class TweetsController < ApplicationController
   def show
     @tweet = Tweet.find(params[:id])
     @user = @tweet.user
+  end
+
+  private
+  
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def check_logged_in
+    redirect_to login_path if current_user.nil?
   end
 end
