ec35b4aeb520a93c37e60c20bb3e90b4632a9d4c
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-12-19 17:22:48 +0900

    rootingの修正と改行コードをLFに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index new create show destroy edit update]
-  resources :tweets,only: %i[index new create show destroy edit update] do
-    resource :favorites, only: [:create, :destroy]
+  resources :tweets, only: %i[index new create show destroy edit update] do
+    resource :favorites, only: %i[create destroy]
   end
 
   get    '/login',   to: 'sessions#new'
