16bd62c01ae2c5a099495cec06da8e5fa55c2375
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-12-20 17:39:11 +0900

    コントローラーにいいね機能を実装

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,16 @@
+class FavoritesController < ApplicationController
+  # POST /tweets/:tweet_id
+  def create
+    # binding.pry
+    favorite = current_user.favorites.build(tweet_id: params[:tweet_id])
+    favorite.save
+    redirect_to tweets_path
+  end
+
+  # DELETE /tweets/:tweet_id
+  def destroy
+    favorite = Favorite.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
+    favorite.destroy
+    redirect_to tweets_path
+  end
+end
