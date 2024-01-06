6dfb94b93edf9148f12d1a1245dbbd85b7cc0ace
Kaito Ono <kaito@KaitonoMacBook-Air.local>
2024-01-06 15:29:26 +0900

    フォロー機能のテストを作成

--- spec/requests/relationships_controller_spec.rb
+++ spec/requests/relationships_controller_spec.rb
@@ -1,77 +1,88 @@
-RSpec.describe FavoritesController, type: :request do
-    describe 'POST /users/:user_id/relationships' do
-      context 'ログインしているとき、ユーザーをフォローした場合' do
-        let!(:user) { create(:user) }
-        let!(:another_user) { create(:user, email: 'another_user@example.com') }
-  
-        before do
-          post login_path, params: { email: user.email, password: user.password }
-        end
-  
-        it 'フォローの数が1増えること' do
-          aggregate_failures do
-            expect do
-              post user_relationships_path(user), params: { user_id: another_user.id }
-            end.to change(Relationship, :count).by(1)
+RSpec.describe RelationshipsController, type: :request do
+  describe 'POST /users/:user_id/relationships' do
+    context 'ログインしているとき、ユーザーをフォローした場合' do
+      let!(:user) { create(:user) }
+      let!(:another_user) { create(:user, email: 'another_user@example.com') }
 
-            # binding.pry
-
-            expect(response).to have_http_status(:found)
-            expect(response).to redirect_to(users_path)
-          end
-        end
+      before do
+        post login_path, params: { email: user.email, password: user.password }
       end
-  
-      context 'ログインしていないとき、ユーザーをフォローした場合' do
-        let!(:user) { create(:user) }
-        let!(:another_user) { create(:user, email: 'another_user@example.com') }
-  
-        it 'フォローの数が変化せず、ログインページにリダイレクトすること' do
-          aggregate_failures do
-            # expect do
-            #   post tweet_favorites_path(tweet), params: { tweet_id: tweet.id }
-            # end.to change(Favorite, :count).by(0)
-            # expect(response).to have_http_status(:found)
-            # expect(response).to redirect_to login_path
-          end
+
+      it 'フォローの数が1増えること' do
+        aggregate_failures do
+          expect do
+            post user_relationships_path(another_user)
+          end.to change(Relationship, :count).by(1)
+          expect(user.active_relationships.count).to eq(1)
+          expect(another_user.passive_relationships.count).to eq(1)
+          expect(response).to have_http_status(:found)
+          expect(response).to redirect_to(users_path)
         end
       end
     end
-  
-    describe 'DELETE /users/:user_id/relationships' do
-      context 'ログインしているとき、ユーザーのフォローを解除した場合' do
-        let!(:user) { create(:user) }
-        let!(:another_user) { create(:user, email: 'another_user@example.com') }
-  
-        before do
-          post login_path, params: { email: favorite.user.email, password: favorite.user.password }
-        end
-  
-        it 'フォローの数が1減ること' do
-          aggregate_failures do
-            # expect do
-            #   delete tweet_favorites_path(favorite.tweet), params: { tweet_id: favorite.tweet.id }
-            # end.to change(Favorite, :count).by(-1)
-            # expect(response).to have_http_status(:found)
-            # expect(response).to redirect_to(tweets_path)
-          end
-        end
-      end
-  
-      context 'ログインしていないとき、ユーザーのフォローを解除した場合' do
-        let!(:user) { create(:user) }
-        let!(:another_user) { create(:user, email: 'another_user@example.com') }
-  
-        it 'フォローの数が変化せず、ログインページにリダイレクトすること' do
-          aggregate_failures do
-            # expect do
-            #   delete tweet_favorites_path(favorite.tweet), params: { tweet_id: favorite.tweet.id }
-            # end.to change(Favorite, :count).by(0)
-            # expect(response).to have_http_status(:found)
-            # expect(response).to redirect_to login_path
-          end
+
+    context 'ログインしていないとき、ユーザーをフォローした場合' do
+      let!(:user) { create(:user) }
+      let!(:another_user) { create(:user, email: 'another_user@example.com') }
+
+      it 'フォローの数が変化せず、ログインページにリダイレクトすること' do
+        aggregate_failures do
+          expect do
+            post user_relationships_path(another_user)
+          end.to change(Relationship, :count).by(0)
+          expect(user.active_relationships.count).to eq(0)
+          expect(another_user.passive_relationships.count).to eq(0)
+          expect(response).to have_http_status(:found)
+          expect(response).to redirect_to login_path
         end
       end
     end
   end
-  
+
+  describe 'DELETE /users/:user_id/relationships' do
+    context 'ログインしているとき、ユーザーのフォローを解除した場合' do
+      let!(:user) { create(:user) }
+      let!(:another_user) { create(:user, email: 'another_user@example.com') }
+
+      before do
+        post login_path, params: { email: user.email, password: user.password }
+        post user_relationships_path(another_user)
+      end
+
+      it 'フォローの数が1減ること' do
+        aggregate_failures do
+          expect do
+            delete user_relationships_path(another_user)
+          end.to change(Relationship, :count).by(-1)
+          expect(user.active_relationships.count).to eq(0)
+          expect(another_user.passive_relationships.count).to eq(0)
+          expect(response).to have_http_status(:found)
+          expect(response).to redirect_to(users_path)
+        end
+      end
+    end
+
+    context 'ログインしていないとき、ユーザーのフォローを解除した場合' do
+      let!(:user) { create(:user) }
+      let!(:another_user) { create(:user, email: 'another_user@example.com') }
+
+      before do
+        post login_path, params: { email: user.email, password: user.password }
+        post user_relationships_path(another_user)
+        delete logout_path
+      end
+
+      it 'フォローの数が変化せず、ログインページにリダイレクトすること' do
+        aggregate_failures do
+          expect do
+            delete user_relationships_path(another_user)
+          end.to change(Relationship, :count).by(0)
+          expect(user.active_relationships.count).to eq(1)
+          expect(another_user.passive_relationships.count).to eq(1)
+          expect(response).to have_http_status(:found)
+          expect(response).to redirect_to login_path
+        end
+      end
+    end
+  end
+end
