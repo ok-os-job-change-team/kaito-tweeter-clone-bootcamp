024c840f95c0c4776b6be23e558fe11f0c7c7e3f
Kaito Ono <kaito@KaitonoMacBook-Air.local>
2024-01-17 17:38:12 +0900

    ログインフォームと会員登録フォームを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
       redirect_to '/users'
     else
       flash.now[:alert] = '登録に失敗しました'
-      render '/users/new'
+      render '/users/new', layout: 'header'
     end
   end
 
