a613b0d8fbd8d4ba6a9a9bfbc04e0316cb73a391
Kaito Ono <kaito@KaitonoMacBook-Air.local>
2024-01-20 10:22:17 +0900

    ログアウトボタンを修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete(:user_id)
-    redirect_to login_path, status: :see_other
+    redirect_to login_path
   end
 end
