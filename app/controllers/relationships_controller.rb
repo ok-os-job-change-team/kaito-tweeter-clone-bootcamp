a7aa4eb1560c611aaaf7f8d89b06910fd694a357
Kaito1290 <sirobu9963@gmail.com>
2024-01-30 10:04:17 +0900

    relationshipsコントローラーを修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -15,7 +15,8 @@ class RelationshipsController < ApplicationController
 
   # DELETE /users/:user_id/relationships
   def destroy
-    follow = find_follow(params[:user_id])
+    follow = current_user.active_relationships.find_by!(follower_id: params[:user_id])
+
     if follow.destroy
       flash[:notice] = 'フォロー解除しました'
       redirect_to users_path
@@ -23,13 +24,5 @@ class RelationshipsController < ApplicationController
       flash.now[:alert] = 'フォロー解除に失敗しました'
       render users_path
     end
-  rescue ActiveRecord::RecordNotFound
-    render users_path
-  end
-
-  private
-
-  def find_follow(target_user_id)
-    current_user.active_relationships.find_by!(follower_id: target_user_id)
   end
 end
