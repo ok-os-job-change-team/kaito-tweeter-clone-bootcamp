a5f174a1d6522b57b2c237ceb4c0ea399298b714
Kaito1290 <sirobu9963@gmail.com>
2024-03-18 11:23:48 +0900

    投稿管理できるようコントローラーとビューを追加

---
+++ app/controllers/admin/tweets_controller.rb
@@ -0,0 +1,22 @@
+class Admin::TweetsController < ApplicationController
+	before_action :check_admin
+
+	# GET /admin/tweets
+	def index
+		@tweets = Tweet.all.preload(:user, :favorites)
+	end
+	
+	# DELETE admin/tweets/:id
+	def destroy
+		@tweet = Tweet.find(params[:id])
+		# @tweet = current_user.tweets.find(params[:id])
+		if @tweet.destroy
+			flash[:notice] = '削除に成功しました'
+			redirect_to admin_tweets_path
+		else
+			flash.now[:alert] = '削除に失敗しました'
+			render admin_tweets_path
+		end
+	end
+end
+  
