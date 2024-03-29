2463ca7ed9677866d9e14679ed901736047cb8ce
Kaito1290 <sirobu9963@gmail.com>
2024-03-29 10:54:21 +0900

    設定ファイルにコメントを追加

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,14 @@
-worker_processes 4
+# 実行プロセス数
+worker_processes 2
 
+# unicornプロセスIDのパス
 pid "/var/run/unicorn.pid"
+
+# Unixドメインソケットのパス(nginx.confの設定と合わせる必要がある)
 listen "/var/tmp/unicorn.sock"
 
+# unicornサーバーの標準出力ログのパス
 stdout_path "./log/unicorn.stdout.log"
+
+# unicornサーバーの標準エラーログのパス
 stderr_path "./log/unicorn.stderr.log"
