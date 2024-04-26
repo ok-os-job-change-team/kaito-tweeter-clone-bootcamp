8a02f20f2d161711c8e5a6058209638c18f7b4c7
Kaito1290 <sirobu9963@gmail.com>
2024-04-26 14:02:19 +0900

    rubocopの指摘箇所を修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,13 +2,14 @@
 worker_processes 2
 
 # unicornプロセスIDのパス
-pid "/var/run/unicorn.pid"
+
+pid '/var/run/unicorn.pid'
 
 # Unixドメインソケットのパス(nginx.confの設定と合わせる必要がある)
-listen "/var/tmp/unicorn.sock"
+listen '/var/tmp/unicorn.sock'
 
 # unicornサーバーの標準出力ログのパス
-stdout_path "./log/unicorn.stdout.log"
+stdout_path './log/unicorn.stdout.log'
 
 # unicornサーバーの標準エラーログのパス
-stderr_path "./log/unicorn.stderr.log"
+stderr_path './log/unicorn.stderr.log'
